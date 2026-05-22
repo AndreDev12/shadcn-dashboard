0e77307d7e49459eec7b61edfc8ece5b80dfcf31
Héctor André <andretorresjava@gmail.com>
2026-05-21 21:36:49 -0500

    Seccion 3: Alert

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,48 @@
+import { InfoIcon, CheckIcon } from 'lucide-react';
+
+import {
+  Alert,
+  AlertAction,
+  AlertDescription,
+  AlertTitle,
+  Button,
+} from '@/components/ui';
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <InfoIcon />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+        <AlertAction>
+          <Button variant="outline">Enable</Button>
+        </AlertAction>
+      </Alert>
+
+      <Alert variant="destructive">
+        <InfoIcon />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+        <AlertAction>
+          <Button variant="outline">Enable</Button>
+        </AlertAction>
+      </Alert>
+
+      <Alert variant="success">
+        <CheckIcon />
+        <AlertTitle>Success</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+        <AlertAction>
+          <Button variant="outline">Enable</Button>
+        </AlertAction>
+      </Alert>
     </div>
   );
 }
